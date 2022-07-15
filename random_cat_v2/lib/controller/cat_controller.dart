65a46ae1cc3195ebbd3d45f06f8894279b621951
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-07-15 22:12:44 +0900

    cats v2

--- random_cat_v2/lib/controller/cat_controller.dart
+++ random_cat_v2/lib/controller/cat_controller.dart
@@ -7,18 +7,13 @@ class CatController extends GetxController {
   String url = "https://api.thecatapi.com/v1/images/search?limit=20";
   var dio = Dio();
 
-  void getCats() async {
+  getCats() async {
+    cats.clear();
     final response = await dio.get(url);
     for (var i = 0; i < response.data.length; i++) {
       final data = response.data;
       Cat cat = Cat(data[i]["id"], data[i]["url"]);
       cats.add(cat);
     }
-  }
-
-  @override
-  void onInit() {
-    getCats();
-    super.onInit();
   }
 }
--- random_cat_v2/lib/widgets/home/home.dart
+++ random_cat_v2/lib/widgets/home/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:random_cat_v2/controller/cat_controller.dart';
 
+import 'main1.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -18,36 +20,9 @@ class Home extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          ImageGrid(),
+          ImageGridLayout(),
         ],
       ),
-    );
-  }
-}
-
-class ImageGrid extends StatelessWidget {
-  const ImageGrid({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: Get.find<CatController>().cats.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemBuilder: (BuildContext context, int index) {
-        return Obx(
-          () => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.network(
-              Get.find<CatController>().cats[index].url,
-              fit: BoxFit.cover,
-            ),
-          ),
-        );
-      },
     );
   }
 }
